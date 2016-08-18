2188c4285f896d272e8fb3a6dcc39278cb360330
Peter Mares <peter@odobo.com>
2016-08-18 17:05:14 +0200

    Location tracking and storage. Now tracks in the background as well

--- LocationTester/ViewController.swift
+++ LocationTester/ViewController.swift
@@ -11,14 +11,23 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    var locMgr: CLLocationManager
+    let distanceKey = "Distance"
+    let defaults = NSUserDefaults.standardUserDefaults()
+    var locMgr = CLLocationManager()
+    var distance: CLLocationDistance = 0.0
+    var lastLocation: CLLocation?
     
     @IBOutlet weak var latitudeLabel: UILabel!
     @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
+    @IBOutlet weak var altitudeLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
     
     required init?(coder aDecoder: NSCoder) {
-        self.locMgr = CLLocationManager()
         super.init(coder: aDecoder)
+        self.locMgr = CLLocationManager()
+        self.distance = loadDistanceTravelled()
+        print(distance)
     }
     
     override func viewDidLoad() {
@@ -40,11 +49,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         }
 
         locMgr.delegate = self
-        locMgr.requestWhenInUseAuthorization()
+        locMgr.requestAlwaysAuthorization()
 
         print("Location Services Enabled: \(CLLocationManager.locationServicesEnabled())")
         print("Heading available: \(CLLocationManager.headingAvailable())")
         
+        locMgr.desiredAccuracy = kCLLocationAccuracyBest
         locMgr.startUpdatingLocation()
         
     }
@@ -53,14 +63,46 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func loadDistanceTravelled() -> CLLocationDistance {
+        distance = defaults.doubleForKey(distanceKey)
+        return distance
+    }
+    
+    func saveDistanceTravelled(d: CLLocationDistance) {
+        defaults.setValue(d, forKey: distanceKey)
+    }
+    
+    func updateDisplay(loc: CLLocation) {
+        latitudeLabel.text = "Latitude: \(loc.coordinate.latitude)"
+        longitudeLabel.text = "Longitude: \(loc.coordinate.longitude)"
+        speedLabel.text = "Speed: \(loc.speed >= 0.0 ? loc.speed : 0.0) m/s"
+        altitudeLabel.text = "Altitude: \(loc.altitude)"
+        distanceLabel.text = "Distance travelled: \(String(format: "%.2f meters", distance))"
+    }
+    
+    // MARK: Actions
+    @IBAction func resetButtonPressed(sender: UIButton) {
+        self.distance = 0.0
+        saveDistanceTravelled(self.distance)
+    }
 
     // MARK: CLLocationManagerDelegate
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print("Locations received: \(locations.count) points")
         for loc in locations {
-            print(loc)
-            latitudeLabel.text = "Latitude: \(loc.coordinate.latitude)"
-            longitudeLabel.text = "Longitude: \(loc.coordinate.longitude)"
+            if loc.speed >= 0.0 {
+                print("1. \(distance)")
+                if let lastLoc = lastLocation {
+                    print("Updating distance")
+                    distance += loc.distanceFromLocation(lastLoc)
+                }
+                print("2. \(distance)")
+                print("last location: \(lastLocation)")
+                self.lastLocation = loc;
+            }
+            
+            updateDisplay(loc)
+            saveDistanceTravelled(distance)
         }
     }
 
